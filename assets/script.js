bc119372869fd08ae8165fbc9f8c9815af1a3b30
Jonathan Ray <jonathanjudsonray@gmail.com>
2023-01-18 17:00:22 -0500

    Finished all the logic and styling

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,31 @@
-var question;
-var enter = document.getElementById("askMe");
-const points = [
-  "It is certain",
-  "Without a doubt",
-  "You may rely on it",
-  "Yes, definitely",
-  "It is decidedly so",
-  "As I see it, yes",
-  "Most likely",
-  "Yes",
-  "Outlook good",
-  "Signs point to yes",
-  "Reply hazy, try again",
-  "Better not tell you now",
-  "Ask again later",
-  "Cannot predict now",
-  "Concentrate and ask again",
-  "Don't count on it",
-  "Outlook not so good",
-  "My sources say no",
-  "Very doubtful",
-  "My reply is no"
+const responses = [
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes - definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful."
 ];
 
-console.log(points);
+function shake() {
+  let randomIndex = Math.floor(Math.random() * responses.length);
+  let responseDiv = document.getElementById("response");
+  responseDiv.innerHTML = responses[randomIndex];
+}
 
-// function myFunction() {
-//   points.sort(function(){return 0.5 - Math.random()});
-//   document.getElementById("response").innerHTML = points[0];
-//   document.getElementById("response").style.fontSize = "18px";
-//   setTimeout(delay, 4000);
-
-//   function delay() {
-//     document.getElementById('response').innerHTML = "8";
-//     document.getElementById("response").style.fontSize = "120px";
-//     document.getElementById("clear").value = "";
-//   }
-// };
-
-function generateAnswer() {
-
-  var response;
-
-    for (let i = 0; i < points; i++); {
-      var random = Math.floor(Math.random() * points.length);
-      console.log(random);
-    };
-
-    function delay() {
-      document.getElementById('response').innerHTML = "8";
-      document.getElementById("response").style.fontSize = "120px";
-      document.getElementById("clear").value = "";
-    };
-    return response;
-};
-
-function response() {
-  var answer = generateAnswer();
-  var answerText = document.getElementById("clear");
-};
-
-enter.addEventListener("click", points);
+const button = document.getElementById("shake-button");
+button.addEventListener("click", shake);
